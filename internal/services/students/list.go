b733713a69e78f8aa0c59380634ca5d4dcc8ac93
krisjanisp <krisjanispetrucena@gmail.com>
2024-02-07 21:47:07 +0200

    correctly listing students

--- internal/services/students/list.go
+++ internal/services/students/list.go
@@ -1,12 +1,49 @@
 package students
 
-import "github.com/KrisjanisP/klase.pps.lv/internal/models"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"os"
+
+	"github.com/KrisjanisP/klase.pps.lv/internal/models"
+)
+
+type tempStudent struct {
+	Names    []string `json:"names"`
+	Password string   `json:"password"`
+}
 
 func ListStudents() []models.Student {
-	return []models.Student{
-		{
-			Names:    []string{},
-			Password: "asdf",
-		},
+	// Open the JSON file
+	file, err := os.Open("./data/students.json")
+	if err != nil {
+		log.Fatalf("Error opening courses.json file: %v", err)
 	}
+	defer file.Close()
+
+	// Read the file content
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatalf("Error reading courses.json file: %v", err)
+	}
+
+	// Unmarshal the JSON data into the tempStudents slice
+	var tempStudents []tempStudent
+	err = json.Unmarshal(data, &tempStudents)
+	if err != nil {
+		log.Fatalf("Error unmarshalling courses data: %v", err)
+	}
+
+	// Map the tempStudents to models.Student
+	var students []models.Student
+	for _, tStudent := range tempStudents {
+		student := models.Student{
+			Names:    tStudent.Names,
+			Password: tStudent.Password,
+		}
+		students = append(students, student)
+	}
+
+	return students
 }
